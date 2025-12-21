8ce6b9b49eae646d247f4d9ca7c893f94bc1fb62
jordyarms <jordan@addwonder.com>
2025-12-20 19:03:45 -0500

    base url update

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jordyarms.github.io",
+    baseUrl: "notes.graphy.ca",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
